d4410ad63d3ee4df046fa4beddcd3f5e6f2aa89b
AFa0304 <c805237@gmail.com>
2019-11-20 09:52:09 +0800

    update

--- component/Event.js
+++ component/Event.js
@@ -1,155 +1,194 @@
-import {isGuid,httpRequest} from '../utils/utils'
+import { isGuid, httpRequest } from '../utils/utils'
 
 export default class Event {
-    constructor(eventID="",idToken="",isBeta=false){
+    constructor(eventID = "", idToken = "", isBeta = false) {
         this.isBeta = isBeta
         this.eventID = eventID
         this.idToken = idToken
-        this.agendaList = getAgendaList(eventID,this.isBeta)
-        this.questList = getQuestList(eventID,this.isBeta)
-        this.luckyDrawList = getLuckyDrawList(eventID,this.isBeta)
+        this.agendaList = getAgendaList(eventID, this.isBeta)
+        this.questList = getQuestList(eventID, this.isBeta)
+        this.luckyDrawList = getLuckyDrawList(eventID, this.isBeta)
+        this.eventData = getEventData(eventID,this.isBeta)
+    }
+    //取得註冊表單
+    getRegQuest(){
+        return new Promise((resolve, reject) => {
+            try {
+                const apiUrl = "/" + this.eventID + "/EventReg"
+                resolve(httpRequest("get", apiUrl, false, {}, [], this.isBeta))
+            } catch (error) {
+                reject(JSON.parse(error.message))
+            }
+        })
+    }
+    //送出註冊表單
+    submitRegQuest(data){
+        return new Promise((resolve, reject) => {
+            try {
+                const apiUrl = "/" + this.eventID + "/EventReg"
+                resolve(httpRequest("post",apiUrl,false,data,[],this.isBeta))
+            } catch (error) {
+                reject(JSON.parse(error.message))
+            }
+        })
     }
     //取得獎品清單
-    getLuckyDrawList(){
-        return new Promise((resolve,reject)=>{
-            try{
-                const apiUrl = "/"+this.eventID+"/LuckyDraw"
-                resolve(httpRequest("get",apiUrl,false,{},[],this.isBeta).Items)
-            }catch(error){
+    getLuckyDrawList() {
+        return new Promise((resolve, reject) => {
+            try {
+                const apiUrl = "/" + this.eventID + "/LuckyDraw"
+                resolve(httpRequest("get", apiUrl, false, {}, [], this.isBeta).Items)
+            } catch (error) {
                 reject(JSON.parse(error.message))
             }
         })
     }
     //查詢中獎物品
-    getDrawItem(){
-        return new Promise((resolve,reject)=>{
-            try{
-                const apiUrl = "/"+this.eventID+"/LuckyDraw/MyDrawItem"
+    getDrawItem() {
+        return new Promise((resolve, reject) => {
+            try {
+                const apiUrl = "/" + this.eventID + "/LuckyDraw/MyDrawItem"
                 const headerConfig = [
                     {
-                        name:"Authorization",
-                        value:"bearer "+this.idToken
+                        name: "Authorization",
+                        value: "bearer " + this.idToken
                     }
                 ]
-                resolve(httpRequest("get",apiUrl,false,{},headerConfig,this.isBeta))
-            }catch(error){
+                resolve(httpRequest("get", apiUrl, false, {}, headerConfig, this.isBeta))
+            } catch (error) {
                 reject(JSON.parse(error.message))
             }
         })
     }
     // 取得Agenda (搜尋ID or 字串 => 若找不到或超過一組則throw error)
-    getAgenda(searchStr=""){
-        return new Promise((resolve,reject)=>{
+    getAgenda(searchStr = "") {
+        return new Promise((resolve, reject) => {
             // 傳入後判斷是否為Guid   是=>搜尋ID   否=>搜尋整個Array
             let agendaID = ""
-            if(isGuid(searchStr)){
-                const result = this.agendaList.find(function(agenda){
+            if (isGuid(searchStr)) {
+                const result = this.agendaList.find(function (agenda) {
                     return agenda.ID === searchStr
                 })
-                result? agendaID=result.ID : reject(new Error("查無此ID"))
+                result ? agendaID = result.ID : reject(new Error("查無此ID"))
             } else {
                 let resultAry = []
-                this.agendaList.map((agenda)=>{
-                    if(JSON.stringify(agenda).indexOf(searchStr)!==-1){
+                this.agendaList.map((agenda) => {
+                    if (JSON.stringify(agenda).indexOf(searchStr) !== -1) {
                         resultAry.push(agenda)
                     }
                     return null
                 })
-                
-                resultAry.length===1? agendaID=resultAry[0].ID : resultAry.length>1?reject(new Error("查到多組包含'"+searchStr+"'的Agenda")) : reject(new Error("查無包含'"+searchStr+"'的Agenda"))
+
+                resultAry.length === 1 ? agendaID = resultAry[0].ID : resultAry.length > 1 ? reject(new Error("查到多組包含'" + searchStr + "'的Agenda")) : reject(new Error("查無包含'" + searchStr + "'的Agenda"))
             }
-            resolve(httpRequest("get","/"+this.eventID+"/Agenda/"+agendaID,false,{},[],this.isBeta))
+            resolve(httpRequest("get", "/" + this.eventID + "/Agenda/" + agendaID, false, {}, [], this.isBeta))
         })
     }
     // 取得Speaker資料
-    getSpeaker(speakerID){
-        return new Promise((resolve,reject)=>{
-            try{
-                const apiUrl = "/"+this.eventID+"/Agenda/Speaker/"+speakerID
-                resolve(httpRequest("get",apiUrl,false,{},[],this.isBeta))
-            }catch(error){
+    getSpeaker(speakerID) {
+        return new Promise((resolve, reject) => {
+            try {
+                const apiUrl = "/" + this.eventID + "/Agenda/Speaker/" + speakerID
+                resolve(httpRequest("get", apiUrl, false, {}, [], this.isBeta))
+            } catch (error) {
                 reject(JSON.parse(error.message))
             }
         })
     }
     // 造訪紀錄
-    setVisitRecord(){
-        const apiUrl = "/"+this.eventID+"/Analytics"
+    setVisitRecord() {
+        const apiUrl = "/" + this.eventID + "/Analytics"
         const data = {
             "FrontSiteURL": window.location.href
         }
-        try{
-            httpRequest("post",apiUrl,false,data,[],this.isBeta)
-        }catch(error){
+        try {
+            httpRequest("post", apiUrl, false, data, [], this.isBeta)
+        } catch (error) {
             console.log(error)
         }
     }
     //登入活動
-    eventLogin(){
-        return new Promise((resolve,reject)=>{
-            try{
+    eventLogin() {
+        return new Promise((resolve, reject) => {
+            try {
                 const apiUrl = "/Account/EventLogin"
                 const data = {
                     "EventID": this.eventID
                 }
                 const headerConfig = [
                     {
-                        name:"Authorization",
-                        value:"bearer "+this.idToken
+                        name: "Authorization",
+                        value: "bearer " + this.idToken
                     }
                 ]
-                resolve(httpRequest("post",apiUrl,false,data,headerConfig,this.isBeta))
-            }catch(error){
+                resolve(httpRequest("post", apiUrl, false, data, headerConfig, this.isBeta))
+            } catch (error) {
                 reject(JSON.parse(error.message))
             }
         })
     }
     //取得Firebase帳戶資料
-    getAccountInfo(){
-        return new Promise((resolve,reject)=>{
-            try{
+    getAccountInfo() {
+        return new Promise((resolve, reject) => {
+            try {
                 const apiUrl = "/Account/Info"
                 const headerConfig = [
                     {
-                        name:"Authorization",
-                        value:"bearer "+this.idToken
+                        name: "Authorization",
+                        value: "bearer " + this.idToken
                     }
                 ]
-                resolve(httpRequest("get",apiUrl,false,{},headerConfig,this.isBeta))
-            }catch(error){
+                resolve(httpRequest("get", apiUrl, false, {}, headerConfig, this.isBeta))
+            } catch (error) {
                 reject(JSON.parse(error.message))
             }
         })
     }
-    getRegData(){
-        return new Promise((resolve,reject)=>{
-            try{
+    getUserRegData() {
+        return new Promise((resolve, reject) => {
+            try {
+                const apiUrl = "/Account/GetUserRegData"
+                const headerConfig = [
+                    {
+                        name: "Authorization",
+                        value: "bearer " + this.idToken
+                    }
+                ]
+                resolve(httpRequest("get", apiUrl, false, {}, headerConfig, this.isBeta))
+            } catch (error) {
+                reject(JSON.parse(error.message))
+            }
+        })
+    }
+    getRegData() {
+        return new Promise((resolve, reject) => {
+            try {
                 const apiUrl = "/Account/RegData"
                 const headerConfig = [
                     {
-                        name:"Authorization",
-                        value:"bearer "+this.idToken
+                        name: "Authorization",
+                        value: "bearer " + this.idToken
                     }
                 ]
-                resolve(httpRequest("get",apiUrl,false,{},headerConfig,this.isBeta))
-            }catch(error){
+                resolve(httpRequest("get", apiUrl, false, {}, headerConfig, this.isBeta))
+            } catch (error) {
                 reject(JSON.parse(error.message))
             }
         })
     }
     //取得QR Code&票卷
-    getTickets(){
-        return new Promise((resolve,reject)=>{
-            try{
+    getTickets() {
+        return new Promise((resolve, reject) => {
+            try {
                 const apiUrl = "/Ticket"
                 const headerConfig = [
                     {
-                        name:"Authorization",
-                        value:"bearer "+this.idToken
+                        name: "Authorization",
+                        value: "bearer " + this.idToken
                     }
                 ]
-                resolve(httpRequest("get",apiUrl,false,{},headerConfig,this.isBeta))
-            }catch(error){
+                resolve(httpRequest("get", apiUrl, false, {}, headerConfig, this.isBeta))
+            } catch (error) {
                 reject(JSON.parse(error.message))
             }
         })
@@ -157,17 +196,22 @@ export default class Event {
 }
 
 //取得獎項清單
-function getLuckyDrawList(eventID,isBeta){
-    const apiUrl = "/"+eventID+"/LuckyDraw"
-    return (httpRequest("get",apiUrl,false,{},[],isBeta).Items)
+function getLuckyDrawList(eventID, isBeta) {
+    const apiUrl = "/" + eventID + "/LuckyDraw"
+    return (httpRequest("get", apiUrl, false, {}, [], isBeta).Items)
 }
 //取得問卷清單
-function getQuestList(eventID,isBeta){
+function getQuestList(eventID, isBeta) {
     const apiUrl = "/" + eventID + "/GetQuest"
-    return (httpRequest("get",apiUrl,false,{},[],isBeta).Items)
+    return (httpRequest("get", apiUrl, false, {}, [], isBeta).Items)
 }
-// 取得Agenda清單
-function getAgendaList(eventID,isBeta){
+//取得Agenda清單
+function getAgendaList(eventID, isBeta) {
     const apiUrl = "/" + eventID + "/Agenda"
-    return (httpRequest("get",apiUrl,false,{},[],isBeta).Agendas)
+    return (httpRequest("get", apiUrl, false, {}, [], isBeta).Agendas)
+}
+//取得Event資料
+function getEventData(eventID,isBeta){
+    const apiUrl = "/Event/" + eventID
+    return (httpRequest("get", apiUrl, false, {}, [], isBeta))
 }
--- component/Quest.js
+++ component/Quest.js
@@ -1,57 +1,57 @@
-import {httpRequest} from '../utils/utils'
+import { httpRequest } from '../utils/utils'
 
 export default class Quest {
-    constructor(questID="",isBeta=false){
+    constructor(questID = "", isBeta = false) {
         this.questID = questID
         this.isBeta = isBeta
     }
     //取得問卷
-    getQuest(){
-        return new Promise((resolve,reject)=>{
-            try{
-                const apiUrl = "/Quest/"+this.questID
-                resolve(httpRequest("get",apiUrl,false,{},[],this.isBeta))
-            }catch(error){
+    getQuest() {
+        return new Promise((resolve, reject) => {
+            try {
+                const apiUrl = "/Quest/" + this.questID
+                resolve(httpRequest("get", apiUrl, false, {}, [], this.isBeta))
+            } catch (error) {
                 reject(JSON.parse(error.message))
             }
         })
     }
     //送出問卷
-    submitQuest(data){
-        return new Promise((resolve,reject)=>{
-            try{
-                const apiUrl = "/Quest/"+this.questID
-                resolve(httpRequest("post",apiUrl,false,data,[],this.isBeta))
-            }catch(error){
+    submitQuest(data) {
+        return new Promise((resolve, reject) => {
+            try {
+                const apiUrl = "/Quest/" + this.questID
+                resolve(httpRequest("post", apiUrl, false, data, [], this.isBeta))
+            } catch (error) {
                 reject(JSON.parse(error.message))
             }
         })
     }
     //Email重複檢查
-    ckeckEmail(email){
-        return new Promise((resolve,reject)=>{
-            try{
-                const apiUrl = "/Quest/"+this.questID+"/CheckEmail"
+    ckeckEmail(email) {
+        return new Promise((resolve, reject) => {
+            try {
+                const apiUrl = "/Quest/" + this.questID + "/CheckEmail"
                 const postData = {
-                    "Email":email
+                    "Email": email
                 }
-                resolve(httpRequest("post",apiUrl,false,postData,[],this.isBeta))
-            }catch(error){
+                resolve(httpRequest("post", apiUrl, false, postData, [], this.isBeta))
+            } catch (error) {
                 reject(JSON.parse(error.message))
             }
         })
     }
     //資料重複檢查
-    checkQuestionUnion(name,value){
-        return new Promise((resolve,reject)=>{
-            try{
-                const apiUrl = "/Quest/"+this.questID+"/CheckQuestionUnion"
+    checkQuestionUnion(name, value) {
+        return new Promise((resolve, reject) => {
+            try {
+                const apiUrl = "/Quest/" + this.questID + "/CheckQuestionUnion"
                 const postData = {
                     "Name": name,
                     "Value": value
                 }
-                resolve(httpRequest("post",apiUrl,false,postData,[],this.isBeta))
-            }catch(error){
+                resolve(httpRequest("post", apiUrl, false, postData, [], this.isBeta))
+            } catch (error) {
                 reject(JSON.parse(error.message))
             }
         })
