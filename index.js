0ad59ec7ebe7d292374dbf99635a2f0a41650b26
AFa0304 <c805237@gmail.com>
2019-11-27 15:47:11 +0800

    export pay

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 export {default as Event} from './component/Event'
 export {default as Quest} from './component/Quest'
+export {default as Pay} from './component/Pay'
 export {default as Trigger} from './component/Trigger'
 
