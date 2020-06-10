54f8eff6f08b4a21629258dbbb3fcd2bfa7120f4
AFa0304 <c805237@gmail.com>
2020-06-10 15:17:22 +0800

    add Draw class

---
+++ component/Draw.js
@@ -0,0 +1,43 @@
+import { alertError, httpRequestPromise, httpRequest } from '../utils/utils'
+
+
+export default class Draw {
+    constructor(eventID = "", drawID = "", idToken = "", isBeta = false) {
+        this.isBeta = isBeta
+        this.eventID = eventID
+        this.drawID = drawID
+        this.idToken = idToken
+    }
+    //取得Draw塗鴉牆
+    getGraffitiWall() {
+        return new Promise((resolve, reject) => {
+            try {
+                const apiUrl = "/" + this.drawID + "/GraffitiWall?EventID=" + this.eventID
+                resolve(httpRequest("get", apiUrl, false, {}, [], this.isBeta))
+            } catch (error) {
+                reject(JSON.parse(error.message))
+            }
+        })
+    }
+    //繪圖
+    drawTextToPng(datas) {
+        return new Promise((resolve, reject) => {
+            const apiUrl = "/" + this.eventID + "/EventReg"
+            httpRequestPromise("post", apiUrl, true, datas, [], this.isBeta).then(response => {
+                resolve(response)
+            }).catch(error => {
+                let jsonErr = null
+                try {
+                    jsonErr = JSON.parse(error)
+                    alertError(jsonErr)
+                    reject(jsonErr)
+                } catch (err) {
+                    //錯誤response非Object時
+                    console.log(err)
+                    alert("送出失敗")
+                    reject(error)
+                }
+            })
+        })
+    }
+}
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 export { default as Event } from './component/Event'
 export { default as Quest } from './component/Quest'
 export { default as Pay } from './component/Pay'
+export { default as Draw } from './component/Draw'
 export { default as Trigger } from './component/Trigger'
 export { default as ChatRoom } from './component/ChatRoom'
 export * from './component/Utils'
