c12373fd3c4aca921960e115f560ae003d6e23b8
AFa0304 <c805237@gmail.com>
2020-10-30 20:36:16 +0800

    submitQuestReview fixed

--- component/Quest.js
+++ component/Quest.js
@@ -41,6 +41,27 @@ export default class Quest {
             })
         })
     }
+    //送出投稿問卷答案
+    submitQuestReview(answer) {
+        const postData = {
+            "ansData": answer
+        }
+        return new Promise((resolve, reject) => {
+            const apiUrl = "/Quest/" + this.questID + "/PostAnswer"
+            let headerConfig = [
+                {
+                    name: "Authorization",
+                    value: "bearer " + this.idToken
+                }
+            ]
+            if (this.apiVersion) { headerConfig.push({ name: "api-version", value: this.apiVersion }) }
+            httpRequestPromise("post", apiUrl, true, postData, headerConfig, this.DomainType, true).then(response => {
+                resolve(response)
+            }).catch(error => {
+                reject(JSON.parse(error))
+            })
+        })
+    }
     //Email重複檢查
     checkEmail(email) {
         return new Promise((resolve, reject) => {
@@ -90,29 +111,6 @@ export default class Quest {
             } catch (error) {
                 reject(JSON.parse(error.message))
             }
-        })
-    }
-    //送出投稿問卷答案
-    submitQuestReview(answers, fileArray = []) {
-        const postData = new FormData()
-        postData.append("AnsJSON", JSON.stringify(answers))
-        for (var i = 0; i < fileArray.length; i++) {
-            postData.append("Files", fileArray[i])
-        }
-        return new Promise((resolve, reject) => {
-            const apiUrl = "/Quest/" + this.questID + "/PostAnswer"
-            let headerConfig = [
-                {
-                    name: "Authorization",
-                    value: "bearer " + this.idToken
-                }
-            ]
-            if (this.apiVersion) { headerConfig.push({ name: "api-version", value: this.apiVersion }) }
-            httpRequestPromise("post", apiUrl, true, postData, headerConfig, this.DomainType, true).then(response => {
-                resolve(response)
-            }).catch(error => {
-                reject(JSON.parse(error))
-            })
         })
     }
     uploadFile(eventID, file, callback = undefined, questionID = "") {
